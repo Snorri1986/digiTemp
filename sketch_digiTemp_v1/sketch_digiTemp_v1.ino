19470b4d298cfe7d465398ac33d42dfe62402cdb
Denys Shabelnyk <dionisiy1986@gmail.com>
2026-09-21 20:55:27 +0200

    #5 - define all pins

--- sketch_digiTemp_v1/sketch_digiTemp_v1.ino
+++ sketch_digiTemp_v1/sketch_digiTemp_v1.ino
@@ -1,7 +1,24 @@
-#include <LiquidCrystal.h> //LCD
-/*----- termo sensor DS18B20 ------ */
+/*----- LCD 16x2 & termo sensor DS18B20 ------ */
+#include <LiquidCrystal.h> 
 #include <OneWire.h>
 #include <DallasTemperature.h>
+
+/*------- LCD --------*/
+#define LCD_RS_PIN  12
+#define LCD_E_PIN   11
+#define LCD_D4_PIN  6
+#define LCD_D5_PIN  7
+#define LCD_D6_PIN  8 
+#define LCD_D7_PIN  9
+
+/*------ LED --------*/
+#define LED_H 4
+#define LED_W 5
+#define LED_N 3
+
+/*-----DS18B20------*/
+#define TMC_F 10
+#define TMC_C 2
 
 void setup() {
   
